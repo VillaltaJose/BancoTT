04d2496f188e74fc141c3ebe27ac6dd19df6acf3
Jose Villalta <josevillalta@vimasistem.com>
2024-07-16 23:44:23 -0500

    Pruebas unitarias form errors

--- src/app/shared/components/form-error/form-error.component.spec.ts
+++ src/app/shared/components/form-error/form-error.component.spec.ts
@@ -1,21 +1,72 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { FormErrorComponent } from './form-error.component';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { By } from '@angular/platform-browser';
 
 describe('FormErrorComponent', () => {
-  let component: FormErrorComponent;
-  let fixture: ComponentFixture<FormErrorComponent>;
+	let component: FormErrorComponent;
+	let fixture: ComponentFixture<FormErrorComponent>;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      declarations: [FormErrorComponent]
-    });
-    fixture = TestBed.createComponent(FormErrorComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+	beforeEach(() => {
+		TestBed.configureTestingModule({
+			declarations: [FormErrorComponent],
+			imports: [ReactiveFormsModule],
+		});
+		fixture = TestBed.createComponent(FormErrorComponent);
+		component = fixture.componentInstance;
+		fixture.detectChanges();
+	});
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+	it('should create', () => {
+		expect(component).toBeTruthy();
+	});
+
+	it('should not show error messages if is not touched or marked as dirty', () => {
+		const control = new FormControl('', { validators: [] });
+		component.control = control;
+
+		component.errors = [
+			{
+				error: 'error1',
+				message: 'Error 1',
+			},
+			{
+				error: 'error2',
+				message: 'Error 2',
+			}
+		];
+
+		control.setErrors({ 'error1': true });
+		fixture.detectChanges();;
+
+		const renderedMessages = fixture.debugElement.queryAll(By.css('.form-error'));
+
+		expect(renderedMessages.length).toBe(0);
+	});
+
+	it('should show error messages when dirty or touched', () => {
+		const control = new FormControl('', { validators: [] });
+		component.control = control;
+
+		component.errors = [
+			{
+				error: 'error1',
+				message: 'Error 1',
+			},
+			{
+				error: 'error2',
+				message: 'Error 2',
+			}
+		];
+
+		control.setErrors({ 'error1': true });
+		control.markAsDirty();
+		fixture.detectChanges();;
+
+		const renderedMessages = fixture.debugElement.queryAll(By.css('.form-error'))
+			.map(el => el.nativeElement.textContent);
+
+		expect(renderedMessages).toEqual(['Error 1']);
+	});
 });
