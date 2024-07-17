fc72e181fa1799e784353dc61c32061002939020
Jose Villalta <josevillalta@vimasistem.com>
2024-07-16 23:45:35 -0500

    Update main-layout.component.spec.ts

--- src/app/layouts/main-layout/main-layout.component.spec.ts
+++ src/app/layouts/main-layout/main-layout.component.spec.ts
@@ -1,21 +1,27 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { MainLayoutComponent } from './main-layout.component';
+import { HeaderModule } from 'src/app/shared/components/header/header.module';
+import { RouterModule } from '@angular/router';
 
 describe('MainLayoutComponent', () => {
-  let component: MainLayoutComponent;
-  let fixture: ComponentFixture<MainLayoutComponent>;
+	let component: MainLayoutComponent;
+	let fixture: ComponentFixture<MainLayoutComponent>;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      declarations: [MainLayoutComponent]
-    });
-    fixture = TestBed.createComponent(MainLayoutComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+	beforeEach(() => {
+		TestBed.configureTestingModule({
+			declarations: [MainLayoutComponent],
+			imports: [
+				HeaderModule,
+				RouterModule,
+			],
+		});
+		fixture = TestBed.createComponent(MainLayoutComponent);
+		component = fixture.componentInstance;
+		fixture.detectChanges();
+	});
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+	it('should create', () => {
+		expect(component).toBeTruthy();
+	});
 });
